0de1ef6b3828da25270b91079d988f072e29bbe1
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-07-02 09:53:15 +0430

    added account methods of user , still in progress

--- src/main/java/server/types/User.java
+++ src/main/java/server/types/User.java
@@ -1,13 +1,17 @@
 package server.types;
- 
+
 import java.io.Serializable;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
-public class User implements Serializable {
+import server.util.Filer;
+import server.util.Identifiable;
+
+public class User implements Serializable, Identifiable {
     private String melliCode, name, passwordHash, email, phone;
-    private ArrayList<Account> accounts;
+    private ArrayList<Account> accounts, fAccounts;
+    private Filer<User> filer;
 
     /**
      * @param melliCode
@@ -17,15 +21,17 @@ public class User implements Serializable {
      * @param phone
      * @throws NoSuchAlgorithmException
      */
-    public User(String melliCode, String name, String password, String email, String phone) throws NoSuchAlgorithmException {
+    public User(String melliCode, String name, String password, String email, String phone)
+            throws NoSuchAlgorithmException {
         this.melliCode = melliCode;
         this.name = name;
         this.email = email;
         this.phone = phone;
         accounts = new ArrayList<Account>();
-        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-        messageDigest.update(password.getBytes());
-        passwordHash = new String(messageDigest.digest());
+        fAccounts = new ArrayList<Account>();
+        setPassword(password);
+        filer = new Filer<User>(this);
+        filer.write();
     }
 
     /**
@@ -43,6 +49,13 @@ public class User implements Serializable {
     }
 
     /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
      * @return the melliCode
      */
     public String getMelliCode() {
@@ -57,10 +70,31 @@ public class User implements Serializable {
     }
 
     /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
      * @return the password
      */
     public String getPasswordHash() {
         return passwordHash;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(password.getBytes());
+            passwordHash = new String(messageDigest.digest());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
     /**
@@ -69,4 +103,41 @@ public class User implements Serializable {
     public String getPhone() {
         return phone;
     }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String getUniqueID() {
+        return melliCode;
+    }
+
+    public void openAccount(String password, boolean isFavorite, String alias) {
+        accounts.add(new Account(this, password, isFavorite, alias));
+    }
+
+    public void deleteAccount(Account account) {
+        if (account.getFavorite())
+            fAccounts.remove(account);
+        accounts.remove(account);
+        account.delete();
+    }
+
+    public void addAccountToFavorites(Account account) {
+        account.setFavorite(true);
+        fAccounts.add(account);
+    }
+
+    public void addAliasTo(Account account, String alias) {
+        account.setAlias(alias);
+    }
+
+    public boolean transfer(Account from, Account to, long val) {
+        from.transfer(to, val);
+    }
+
 }
