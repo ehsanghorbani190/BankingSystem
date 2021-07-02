1a1a4762c2c699a0a6d36b88685f990a344c04fb
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-07-02 09:52:49 +0430

    Completed account class

--- src/main/java/server/types/Account.java
+++ src/main/java/server/types/Account.java
@@ -5,14 +5,17 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
+import server.util.Filer;
+import server.util.Identifiable;
 
-public class Account implements Serializable {
+
+public class Account implements Serializable, Identifiable {
     private String id, passwordHash;
     private long balance;
     private boolean isFavorite;
     private User owner;
     private String alias;
-
+    private Filer<Account> filer;
     public Account(User owner, String password, boolean isFavorite, String alias) throws NoSuchAlgorithmException {
         this.balance = 0;
         this.isFavorite = isFavorite;
@@ -23,6 +26,8 @@ public class Account implements Serializable {
         MessageDigest messageDigest = MessageDigest.getInstance("MD5");
         messageDigest.update(password.getBytes());
         passwordHash = new String(messageDigest.digest());
+        filer = new Filer<Account>(this);
+        filer.write();
     }
 
     /**
@@ -69,11 +74,39 @@ public class Account implements Serializable {
     */
    public void setFavorite(boolean isFavorite) {
        this.isFavorite = isFavorite;
+       filer.update(this);
    }
    /**
     * @param alias the alias to set
     */
    public void setAlias(String alias) {
        this.alias = alias;
+       filer.update(this);
+   }
+   public boolean delete() {
+        return filer.delete();
+   }
+
+   public boolean addMoney(long val){
+       if(val < 0) return false;
+       balance += val;
+       filer.update(this);
+       return true;
+   }
+
+   public boolean getMoney(long val){
+        if(val > balance || val < 0) return false;
+        balance -= val;
+        filer.update(this);
+        return true;
+   }
+
+   public boolean transfer(Account to , long val) {
+       return to.addMoney(val) && this.getMoney(val);
+       
+   }
+   @Override
+   public String getUniqueID() {
+       return id;
    }
 }
