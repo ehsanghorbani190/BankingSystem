d87ef138a89b6a1bbadc0d85d36165ec4f1fa4db
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-07-06 12:22:48 +0430

    Connected transfer to server

--- src/client/MoneyTransfer.java
+++ src/client/MoneyTransfer.java
@@ -1,6 +1,5 @@
 package client;
 
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -42,8 +41,7 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 import util.DataDealer;
 
-public class MoneyTransfer
-{
+public class MoneyTransfer {
     MoneyTransfer(Stage primaryStage) throws FileNotFoundException {
         Group root = new Group();
         Scene scene = new Scene(root);
@@ -71,11 +69,10 @@ public class MoneyTransfer
         rect.setLayoutX(30);
         rect.setStroke(Color.BLACK);
         rect.setFill(Color.WHITE);
-        rect.setFill(new LinearGradient(
-                0, 0, 1, 1, true,                      //sizing
-                CycleMethod.NO_CYCLE,                  //cycling
-                new Stop(0, Color.web("#FFFFFF")),     //colors
-                new Stop(1, Color.web("#F5F5F5")))//  #ADD8E6
+        rect.setFill(new LinearGradient(0, 0, 1, 1, true, // sizing
+                CycleMethod.NO_CYCLE, // cycling
+                new Stop(0, Color.web("#FFFFFF")), // colors
+                new Stop(1, Color.web("#F5F5F5")))// #ADD8E6
         );
         root.getChildren().add(rect);
 
@@ -85,54 +82,61 @@ public class MoneyTransfer
         text.setFill(Color.GRAY);
         root.getChildren().add(text);
 
-
         ChoiceBox scCard = new ChoiceBox();
-        scCard.setLayoutY(text.getLayoutY()+10);
+        scCard.setLayoutY(text.getLayoutY() + 10);
         scCard.setLayoutX(80);
         root.getChildren().add(scCard);
         DataDealer d = new DataDealer(2);
         Client.ch.send(d);
         d = Client.ch.recieve();
-        if(d.getStatus() == 202){
+        if (d.getStatus() == 202) {
             String id = d.getData("0");
-            for (int i = 1; id != null ; i++) {
+            for (int i = 1; id != null; i++) {
                 scCard.getItems().add(id);
                 id = d.getData(String.valueOf(i));
             }
-        }
-        else{
+        } else {
             Alert a = new Alert(AlertType.WARNING);
             a.setTitle("Warning");
             a.setContentText(d.getError());
         }
-       // scCard.setPromptText("Source card");
+        // scCard.setPromptText("Source card");
 
         Text text2 = new Text("password:");
         text2.setLayoutX(80);
-        text2.setLayoutY(scCard.getLayoutY()+50);
+        text2.setLayoutY(scCard.getLayoutY() + 50);
         text2.setFill(Color.GRAY);
         root.getChildren().add(text2);
 
         PasswordField scPass = new PasswordField();
-        scPass.setLayoutY(text2.getLayoutY()+10);
+        scPass.setLayoutY(text2.getLayoutY() + 10);
         scPass.setLayoutX(80);
         root.getChildren().add(scPass);
         scPass.setPromptText("password");
 
+        Text text4 = new Text("Value:");
+        text4.setLayoutX(80);
+        text4.setLayoutY(scPass.getLayoutY() + 50);
+        text4.setFill(Color.GRAY);
+        root.getChildren().add(text4);
+
+        TextField amount = new TextField();
+        amount.setLayoutY(text4.getLayoutY() + 10);
+        amount.setLayoutX(80);
+        root.getChildren().add(amount);
+        amount.setPromptText("value to transfer");
+
         Text text3 = new Text("Destination card number:");
         text3.setLayoutX(80);
-        text3.setLayoutY(scPass.getLayoutY()+50);
+        text3.setLayoutY(amount.getLayoutY() + 50);
         text3.setFill(Color.GRAY);
         root.getChildren().add(text3);
 
-
         TextField dsCard = new TextField();
-        dsCard.setLayoutY(text3.getLayoutY()+10);
+        dsCard.setLayoutY(text3.getLayoutY() + 10);
         dsCard.setLayoutX(80);
         root.getChildren().add(dsCard);
         dsCard.setPromptText("destination card");
-
-
 
         Button ok = new Button();
         ok.setTranslateX(245);
@@ -140,18 +144,18 @@ public class MoneyTransfer
 
         InputStream input3 = new FileInputStream("./icons/next.png");
         Image background3 = new Image(input3);
-        ok.setBackground(new Background(new BackgroundImage(background3 , BackgroundRepeat.REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
-        ok.setPadding(new Insets(15,0,0,30));
-
-
+        ok.setBackground(new Background(new BackgroundImage(background3, BackgroundRepeat.REPEAT,
+                BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
+        ok.setPadding(new Insets(15, 0, 0, 30));
 
         root.getChildren().add(ok);
 
         Button menu = new Button();
         InputStream input4 = new FileInputStream("./icons/mennu.png");
         Image background4 = new Image(input4);
-        menu.setBackground(new Background(new BackgroundImage(background4 , BackgroundRepeat.REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
-        menu.setPadding(new Insets(15,10,0,25));
+        menu.setBackground(new Background(new BackgroundImage(background4, BackgroundRepeat.REPEAT,
+                BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
+        menu.setPadding(new Insets(15, 10, 0, 25));
         menu.setTranslateY(0);
         menu.setLayoutX(267);
         root.getChildren().add(menu);
@@ -170,61 +174,67 @@ public class MoneyTransfer
             @Override
             public void handle(ActionEvent event) {
 
-                if(dsCard.getText().isEmpty() || scCard.getItems().isEmpty())
-                {
+                if (dsCard.getText().isEmpty() || scCard.getItems().isEmpty()) {
 
-                    if(scCard.getItems().isEmpty())
-                    {
-                        scCard.setStyle("-fx-border-color: #800000;" +
-                                "    -fx-border-width: 1px;" +
-                                "    -fx-border-style: solid;");
+                    if (scCard.getItems().isEmpty()) {
+                        scCard.setStyle("-fx-border-color: #800000;" + "    -fx-border-width: 1px;"
+                                + "    -fx-border-style: solid;");
                     }
-                    if(scPass.getText().isEmpty())
-                    {
-                        scPass.setStyle("-fx-border-color: #800000;" +
-                                "    -fx-border-width: 1px;" +
-                                "    -fx-border-style: solid;");
+                    if (scPass.getText().isEmpty()) {
+                        scPass.setStyle("-fx-border-color: #800000;" + "    -fx-border-width: 1px;"
+                                + "    -fx-border-style: solid;");
                     }
-                    if(dsCard.getText().isEmpty())
-                    {
-                        dsCard.setStyle("-fx-border-color: #800000;" +
-                                "    -fx-border-width: 1px;" +
-                                "    -fx-border-style: solid;");
+                    if (dsCard.getText().isEmpty()) {
+                        dsCard.setStyle("-fx-border-color: #800000;" + "    -fx-border-width: 1px;"
+                                + "    -fx-border-style: solid;");
                     }
-
+                    if (amount.getText().isEmpty()) {
+                        amount.setStyle("-fx-border-color: #800000;" + "    -fx-border-width: 1px;"
+                                + "    -fx-border-style: solid;");
+                    }
 
                     Alert alert = new Alert(Alert.AlertType.WARNING);
                     alert.setTitle("Warning");
                     alert.setHeaderText("Required Fields Empty");
-                    alert.setContentText("The fields highlighted in red must be filled "
-                            + "out.\nPlease try again.");
+                    alert.setContentText("The fields highlighted in red must be filled " + "out.\nPlease try again.");
                     alert.showAndWait();
 
-                    scCard.setStyle("-fx-border-color: #FFFAFA;" +
-                            "    -fx-border-width: 0px;" +
-                            "    -fx-border-style: solid;");
+                    scCard.setStyle("-fx-border-color: #FFFAFA;" + "    -fx-border-width: 0px;"
+                            + "    -fx-border-style: solid;");
 
-                    scPass.setStyle("-fx-border-color: #FFFAFA;" +
-                            "    -fx-border-width: 0px;" +
-                            "    -fx-border-style: solid;");
+                    scPass.setStyle("-fx-border-color: #FFFAFA;" + "    -fx-border-width: 0px;"
+                            + "    -fx-border-style: solid;");
 
-                    dsCard.setStyle("-fx-border-color: #FFFAFA;" +
-                            "    -fx-border-width: 0px;" +
-                            "    -fx-border-style: solid;");
+                    dsCard.setStyle("-fx-border-color: #FFFAFA;" + "    -fx-border-width: 0px;"
+                            + "    -fx-border-style: solid;");
+                    amount.setStyle("-fx-border-color: #FFFAFA;" + "    -fx-border-width: 0px;"
+                            + "    -fx-border-style: solid;");
+                } else {
+                    DataDealer req = new DataDealer(8);
+                    req.addData("id", (String) scCard.getValue());
+                    req.addData("value", amount.getText());
+                    req.addData("password", scPass.getText());
+                    req.addData("dest", dsCard.getText());
+                    Client.ch.send(req);
+                    DataDealer res = Client.ch.recieve();
+                    if (res.getStatus() == 208) {
+                        Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
+                        alert2.setTitle("Successful");
+                        alert2.setHeaderText("Successfully transfered money from " + (String) scCard.getValue() + " to "
+                                + dsCard.getText());
+                        alert2.setContentText("You can see the changes in the 'Account Information' section");
+                        alert2.showAndWait();
+                    } else {
+                        Alert a = new Alert(AlertType.ERROR);
+                        a.setTitle("Error");
+                        a.setHeaderText("Error " + res.getStatus());
+                        a.setContentText(res.getError());
+                        a.showAndWait();
+                    }
                 }
-               else
-                {
-                    Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
-                    alert2.setTitle("Successfuly");
-                    alert2.setHeaderText("Successful money transfer");
-                    alert2.setContentText("You can see the changes in the 'Account Information' section");
-                    alert2.showAndWait();
-                }
-
 
             }
         });
-
 
         primaryStage.setTitle("money transfer");
         primaryStage.setHeight(480);
@@ -235,10 +245,9 @@ public class MoneyTransfer
 
         Text clock = new Text();
         DateFormat format = DateFormat.getInstance();
-        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1),new EventHandler<ActionEvent>() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event)
-            {
+            public void handle(ActionEvent event) {
                 Calendar cal = Calendar.getInstance();
                 clock.setText(format.format(cal.getTime()));
                 clock.setFill(Color.BLACK);
@@ -248,8 +257,6 @@ public class MoneyTransfer
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
-
-
 
         VBox vx = new VBox(clock);
         vx.setLayoutX(90);
